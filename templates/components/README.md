dfe000b14d8b594b55694bc16e6a3ce0f12c863d
Jimmy Moon <ragingwind@gmail.com>
2020-03-30 07:15:52 +0900

    Update blog components

---
+++ templates/components/README.md
@@ -0,0 +1,7 @@
+# @next-template/components
+
+> Reusable React components for @next-template. heavily inspored @zeit blog-starer project
+
+# License
+
+MIT @ Jimmy Moon
---
+++ templates/components/blog/index.tsx
@@ -0,0 +1,15 @@
+import Container from './container'
+import Footer from './footer'
+import Hero from './hero'
+import Layout from './layout'
+import Post from './post'
+import Posts from './posts'
+
+export {
+  Container,
+  Footer,
+  Hero,
+  Layout,
+  Post,
+  Posts
+}
